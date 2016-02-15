97322726e2c84aacdcaecc38fe0fa931caf9e51e
Minh <admin@MacBook-Pro-de-admin.local>
2016-02-15 21:36:12 +0100

    add sub category

--- MyWardrope/NewSubCategoryVC.swift
+++ MyWardrope/NewSubCategoryVC.swift
@@ -1,11 +1,9 @@
 //
-//  Category.swift
+//  NewSubCategoryVC.swift
 //  MyWardrope
 //
-//  Created by Minh on 13.02.16.
+//  Created by Minh on 15.02.16.
 //  Copyright © 2016 NAT. All rights reserved.
 //
 
 import Foundation
-
-
---
+++ MyWardrope/SubCategoryTableVC.swift
@@ -0,0 +1,9 @@
+//
+//  SubCategoryTableVC.swift
+//  MyWardrope
+//
+//  Created by Minh on 15.02.16.
+//  Copyright © 2016 NAT. All rights reserved.
+//
+
+import Foundation
